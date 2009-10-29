2d98565beb5ce482fb4ebe09da0a372ccf149ed4
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2009-10-29 18:46:20 +0000

    Update jar versions.

--- vt-peoplesearch/trunk/bin/peoplesearch.bat
+++ vt-peoplesearch/trunk/bin/peoplesearch.bat
@@ -9,7 +9,7 @@ set JAVA=%JAVA_HOME%\bin\java
 set PEOPLESEARCH_JAR=%VTPEOPLESEARCH_HOME%\jars\vt-peoplesearch-${project.version}.jar
 set LIBDIR=%VTPEOPLESEARCH_HOME%\lib
 
-set CLASSPATH=%LIBDIR%\aopalliance-1.0.jar;%LIBDIR%\commons-cli-1.2.jar;%LIBDIR%\commons-codec-1.3.jar;%LIBDIR%\commons-logging-1.1.1.jar;%LIBDIR%\dom4j-1.6.1.jar;%LIBDIR%\spring-beans-2.5.6.jar;%LIBDIR%\spring-context-2.5.6.jar;%LIBDIR%\spring-core-2.5.6.jar;%LIBDIR%\vt-ldap-3.1.jar;%LIBDIR%\vt-servlet-filters-2.0.1.jar;%PEOPLESEARCH_JAR%
+set CLASSPATH=%LIBDIR%\aopalliance-1.0.jar;%LIBDIR%\commons-cli-1.2.jar;%LIBDIR%\commons-codec-1.3.jar;%LIBDIR%\commons-logging-1.1.1.jar;%LIBDIR%\dom4j-1.6.1.jar;%LIBDIR%\spring-beans-2.5.6.jar;%LIBDIR%\spring-context-2.5.6.jar;%LIBDIR%\spring-core-2.5.6.jar;%LIBDIR%\vt-ldap-3.2.jar;%LIBDIR%\vt-servlet-filters-2.0.2.jar;%PEOPLESEARCH_JAR%
 
 call "%JAVA%" -cp "%CLASSPATH%" edu.vt.middleware.ldap.search.PeopleSearch %*
 goto end
