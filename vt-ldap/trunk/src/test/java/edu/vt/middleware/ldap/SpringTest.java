b445ae6e74a52d8d541054b39eb43f3a675f4cc4
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2010-06-27 16:39:46 +0000

    Add spring test for creating Ldap objects via application context.

---
+++ vt-ldap/trunk/src/test/java/edu/vt/middleware/ldap/SpringTest.java
@@ -0,0 +1,58 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2010 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package edu.vt.middleware.ldap;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import edu.vt.middleware.ldap.pool.BlockingLdapPool;
+  
+/**
+ * Unit test for Spring integration.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$
+ */
+public class SpringTest
+{
+
+
+  /**
+   * Attempts to load all Spring application context XML files to
+   * verify proper wiring.
+   *
+   * @throws  Exception  On test failure.
+   */
+  @Test(groups = {"ldaptest"})
+  public void testSpringWiring()
+    throws Exception
+  {
+    final ClassPathXmlApplicationContext context =
+      new ClassPathXmlApplicationContext(new String[] {
+        "/spring-context.xml",
+      });
+    AssertJUnit.assertTrue(context.getBeanDefinitionCount() > 0);
+    final Ldap l = (Ldap) context.getBean("ldap");
+    l.close();
+
+    final ClassPathXmlApplicationContext poolContext =
+      new ClassPathXmlApplicationContext(new String[] {
+        "/spring-pool-context.xml",
+      });
+    AssertJUnit.assertTrue(poolContext.getBeanDefinitionCount() > 0);
+    final BlockingLdapPool lp =
+      (BlockingLdapPool) poolContext.getBean("ldapPool");
+    lp.close();
+  }
+}
