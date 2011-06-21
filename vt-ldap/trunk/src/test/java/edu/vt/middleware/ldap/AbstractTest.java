bcdb5f0b13073c4e5e1e680e8bd06ba1ef60636a
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2011-06-21 18:15:18 +0000

    3.x and 4.x differ in that 4.x wraps input in LdapName. This means that input escaping is slightly less strict, but escaped characters can still be returned from JNDI.

--- vt-ldap/trunk/src/test/java/edu/vt/middleware/ldap/AbstractTest.java
+++ vt-ldap/trunk/src/test/java/edu/vt/middleware/ldap/AbstractTest.java
@@ -42,8 +42,9 @@ public abstract class AbstractTest
     conn.open();
     final CompareOperation compare = new CompareOperation(conn);
     final LdapAttribute la = new LdapAttribute();
-    la.setName(entry.getDn().split(",ou=")[0].split("=")[0]);
-    la.addStringValue(entry.getDn().split(",ou=")[0].split("=")[1]);
+    la.setName(entry.getDn().split(",ou=")[0].split("=", 2)[0]);
+    la.addStringValue(
+      entry.getDn().split(",ou=")[0].split("=", 2)[1].replaceAll("\\\\", ""));
     while (
       !compare.execute(new CompareRequest(entry.getDn(), la)).getResult()) {
       Thread.sleep(100);
