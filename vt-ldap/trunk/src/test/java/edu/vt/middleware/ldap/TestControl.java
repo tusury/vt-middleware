5624a0dd6698ab2e265907e6e775d950c80a6210
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2011-08-10 18:28:01 +0000

    Use an ldap attribute as a semaphore for blocking if parallel tests run.

---
+++ vt-ldap/trunk/src/test/java/edu/vt/middleware/ldap/TestControl.java
@@ -0,0 +1,88 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2010 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package edu.vt.middleware.ldap;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Contains functions that run before and after all tests.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$
+ */
+public class TestControl
+{
+
+  /** DN to block on. */
+  public static final String DN = "uid=1,ou=test,dc=vt,dc=edu";
+
+  /** Attribute to block on. */
+  public static final LdapAttribute ATTR_IDLE =
+    new LdapAttribute("mail", "test-idle@vt.edu");
+
+  /** Attribute to block on. */
+  public static final LdapAttribute ATTR_RUNNING =
+    new LdapAttribute("mail", "test-running@vt.edu");
+
+  /** Time to wait before checking if lock is available. */
+  public static final int WAIT_TIME = 5000;
+
+
+  /**
+   * Obtains the lock before running all tests.
+   *
+   * @throws Exception on test failure
+   */
+  @BeforeSuite(alwaysRun = true)
+  public void setup()
+    throws Exception
+  {
+    final Connection conn = TestUtil.createSetupConnection();
+    conn.open();
+    // wait for other tests to finish
+    int i = 1;
+    while (!conn.compare(DN, ATTR_IDLE)) {
+      System.err.println("Waiting for test lock...");
+      Thread.sleep(WAIT_TIME * i++);
+    }
+    conn.modify(
+      DN,
+      new AttributeModification[] {
+        new AttributeModification(
+          AttributeModificationType.REPLACE, ATTR_RUNNING), });
+    conn.close();
+  }
+
+
+  /**
+   * Releases the lock after running all tests.
+   *
+   * @throws Exception on test failure
+   */
+  @AfterSuite(alwaysRun = true)
+  public void teardown()
+    throws Exception
+  {
+    final Connection conn = TestUtil.createSetupConnection();
+    conn.open();
+    // set attribute when tests are finished
+    conn.modify(
+      DN,
+      new AttributeModification[] {
+        new AttributeModification(
+          AttributeModificationType.REPLACE, ATTR_IDLE), });
+    conn.close();
+  }
+}
