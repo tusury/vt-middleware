3c22b1509ec3e10cca9ae2e8582af0c943e66520
marvin.addison <marvin.addison@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2011-08-25 11:32:28 +0000

    http://code.google.com/p/vt-middleware/issues/detail?id=117 Move generation of log4j XML configuration into log4j module, which precluded use of JSP.  Switch to StAX-based XML generation and provide capability to pretty-print XML for convenient reading.

--- gator/trunk/gator-integration-log4j/src/main/java/edu/vt/middleware/gator/log4j/web/XmlConfigViewController.java
+++ gator/trunk/gator-integration-log4j/src/main/java/edu/vt/middleware/gator/log4j/web/XmlConfigViewController.java
@@ -13,14 +13,24 @@
 */
 package edu.vt.middleware.gator.log4j.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 import edu.vt.middleware.gator.AppenderConfig;
+import edu.vt.middleware.gator.AppenderParamConfig;
+import edu.vt.middleware.gator.CategoryConfig;
+import edu.vt.middleware.gator.LayoutParamConfig;
 import edu.vt.middleware.gator.ParamConfig;
 import edu.vt.middleware.gator.ProjectConfig;
 import edu.vt.middleware.gator.server.SocketServer;
 import edu.vt.middleware.gator.util.FileHelper;
+import edu.vt.middleware.gator.util.StaxIndentationHandler;
 import edu.vt.middleware.gator.web.AbstractController;
 import edu.vt.middleware.gator.web.ControllerHelper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,49 +46,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class XmlConfigViewController extends AbstractController
 {
+  /** log4j namespace. */
+  public static final String NS_LOG4J = "http://jakarta.apache.org/log4j/";
 
-  /** IP address socket server is bound to. */
-  protected String bindAddress = SocketServer.DEFAULT_BIND_ADDRESS;
+  /** Name of socket appender. */
+  public static final String SOCKET_APPENDER_NAME = "SOCKET";
 
-  /** Port socket server will listen on. */
-  protected int port = SocketServer.DEFAULT_PORT;
-
-
-  /**
-   * Sets the bind address on which incoming connections will be accepted.
-   *
-   * @param  ipAddress  Dotted IP address of bind address.
-   */
-  public void setBindAddress(final String ipAddress)
-  {
-    bindAddress = ipAddress;
-  }
-
-
-  /**
-   * Sets the port on which to listen for client connections.
-   *
-   * @param  n  Listening port number.
-   */
-  public void setPort(final int n)
-  {
-    this.port = n;
-  }
+  /** Server instance. */
+  @Autowired
+  private SocketServer socketServer;
 
 
   @RequestMapping(
     value = "/project/{projectName}/log4j.xml",
     method = RequestMethod.GET
   )
-  public String getLog4jXml(
+  public void getLog4jXml(
     @PathVariable("projectName") final String projectName,
-    final Model model)
+    final Model model,
+    final HttpServletRequest request,
+    final HttpServletResponse response) throws Exception
   {
     ProjectConfig project = getProject(projectName);
-    // Update file appender paths to be suitable for clients
     // Work on a clone so the original is unchanged
     project = ControllerHelper.cloneProject(project);
+
+    final XMLStreamWriter writer =
+      StaxIndentationHandler.createIndentingStreamWriter(
+          response.getOutputStream());
+    writer.writeStartDocument("UTF-8", "1.0");
+    writer.writeStartElement("log4j", "configuration", NS_LOG4J);
+    writer.writeStartElement("appender");
+    writer.writeAttribute("name", SOCKET_APPENDER_NAME);
+    writer.writeAttribute("class", "org.apache.log4j.net.SocketAppender");
+    writeParam(writer, "RemoteHost", socketServer.getBindAddress());
+    writeParam(writer, "Port", socketServer.getPort());
+    writeParam(writer, "ReconnectionDelay", "60000");
+    writeParam(writer, "Threshold", "ALL");
+    writer.writeEndElement(); //appender
+   
     for (AppenderConfig appender : project.getAppenders()) {
+      // Update file appender paths to be suitable for clients
       final ParamConfig fileParam = appender.getAppenderParam("file");
       if (fileParam != null) {
         final String clientAppenderPath = FileHelper.pathCat(
@@ -86,10 +94,77 @@ public class XmlConfigViewController extends AbstractController
           fileParam.getValue());
         fileParam.setValue(clientAppenderPath);
       }
+      writeAppender(writer, appender);
     }
-    model.addAttribute("project", project);
-    model.addAttribute("bindAddress", bindAddress);
-    model.addAttribute("port", port);
-    return "log4jXml";
+    
+    for (CategoryConfig category : project.getCategories()) {
+      writeCategory(writer, category);
+    }
+    
+    writer.writeEndElement(); //configuration
+    writer.writeEndDocument();
+    writer.close();
+  }
+  
+
+  private void writeParam(
+    final XMLStreamWriter w, final String name, final Object value)
+    throws XMLStreamException
+  {
+    w.writeEmptyElement("param");
+    w.writeAttribute("name", name);
+    w.writeAttribute("value", value.toString());
+  }
+
+
+  private void writeAppender(
+    final XMLStreamWriter w, final AppenderConfig appender)
+    throws XMLStreamException
+  {
+    w.writeStartElement("appender");
+    w.writeAttribute("name", appender.getName());
+    w.writeAttribute("class", appender.getAppenderClassName());
+    for (AppenderParamConfig p : appender.getAppenderParams()) {
+      writeParam(w, p.getName(), p.getValue());
+    }
+    if (appender.getErrorHandlerClassName() != null) {
+      w.writeEmptyElement("errorHandler");
+      w.writeAttribute("class", appender.getErrorHandlerClassName());
+    }
+    if (appender.getLayoutClassName() != null) {
+      w.writeStartElement("layout");
+      w.writeAttribute("class", appender.getLayoutClassName());
+      for (LayoutParamConfig p : appender.getLayoutParams()) {
+        writeParam(w, p.getName(), p.getValue());
+      }
+      w.writeEndElement();
+    }
+    w.writeEndElement();
+  }
+
+
+  private void writeCategory(
+    final XMLStreamWriter w, final CategoryConfig category)
+    throws XMLStreamException
+  {
+    if (category.isRoot()) {
+      w.writeStartElement("root");
+    } else {
+      w.writeStartElement("category");
+      w.writeAttribute("name", category.getName());
+      w.writeAttribute("additivity",
+          Boolean.toString(category.getAdditivity()));
+    }
+    w.writeEmptyElement("priority");
+    w.writeAttribute("value", category.getLevel());
+    for (AppenderConfig p : category.getAppenders()) {
+      w.writeEmptyElement("appender-ref");
+      w.writeAttribute("ref", p.getName());
+    }
+    if (category.isAllowSocketAppender()) {
+      w.writeEmptyElement("appender-ref");
+      w.writeAttribute("ref", SOCKET_APPENDER_NAME);
+    }
+    w.writeEndElement();
   }
 }
