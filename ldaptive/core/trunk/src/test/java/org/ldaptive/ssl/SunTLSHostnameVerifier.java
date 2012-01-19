bf7f25e675a5e0fe8c6f6155f61b2e7637e3fe51
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2012-01-19 02:29:56 +0000

    LDAPS does not perform hostname verification with the JNDI provider. Update TLSSocketFactory to support hostname verification. Add a DefaultHostnameVerifier which closely resembles how startTLS performs hostname verification. Update JndiConnectionFactory to use the DefaultHostnameVerifier when SSL is requested and no socket factory is set. Update the SingletonTLSSocketFactory to include additional instance variables.

---
+++ ldaptive/core/trunk/src/test/java/org/ldaptive/ssl/SunTLSHostnameVerifier.java
@@ -0,0 +1,72 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2012 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.ssl;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+// CheckStyle:IllegalImport OFF
+import sun.security.util.HostnameChecker;
+// CheckStyle:IllegalImport ON
+
+/**
+ * A {@link HostnameVerifier} that delegates to the internal Sun implementation
+ * at sun.security.util.HostnameChecker. This is the implementation used by
+ * JNDI with StartTLS.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class SunTLSHostnameVerifier implements HostnameVerifier
+{
+
+
+  /** {@inheritDoc} */
+  @Override
+  public boolean verify(final String hostname, final SSLSession session)
+  {
+    boolean b = false;
+    try {
+      b = verify(hostname, (X509Certificate) session.getPeerCertificates()[0]);
+    } catch (SSLPeerUnverifiedException e) {
+      b = false;
+    }
+    return b;
+  }
+
+
+  /**
+   * Expose convenience method for testing.
+   *
+   * @param  hostname  to verify
+   * @param  cert  to verify hostname against
+   *
+   * @return  whether the certificate is allowed
+   */
+  public boolean verify(final String hostname, final X509Certificate cert)
+  {
+    boolean b = false;
+    final HostnameChecker checker = HostnameChecker.getInstance(
+      HostnameChecker.TYPE_LDAP);
+    try {
+      checker.match(hostname, cert);
+      b = true;
+    } catch (CertificateException e) {
+      b = false;
+    }
+    return b;
+  }
+}
