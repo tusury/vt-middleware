69130d588652be45b6ae5f8072a46f2ffe7e90b3
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2012-03-23 20:33:53 +0000

    Move startTLS functionality into a separate class. Leverage connect and response timeout properties in LDAPConnectionOptions. Change the SearchIterator to use a synchronous operation.

--- ldaptive/unboundid-provider/trunk/src/main/java/org/ldaptive/provider/unboundid/UnboundIdStartTLSConnectionFactory.java
+++ ldaptive/unboundid-provider/trunk/src/main/java/org/ldaptive/provider/unboundid/UnboundIdStartTLSConnectionFactory.java
@@ -14,26 +14,34 @@
 package org.ldaptive.provider.unboundid;
 
 import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
+import com.unboundid.ldap.sdk.ExtendedResult;
 import com.unboundid.ldap.sdk.LDAPConnection;
 import com.unboundid.ldap.sdk.LDAPConnectionOptions;
 import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.ResultCode;
+import com.unboundid.ldap.sdk.extensions.StartTLSExtendedRequest;
 import org.ldaptive.LdapException;
 import org.ldaptive.LdapURL;
 import org.ldaptive.provider.AbstractConnectionFactory;
 import org.ldaptive.provider.ConnectionException;
 
 /**
- * Creates ldap connections using the UnboundId LDAPConnection class.
+ * Creates ldap connections using the UnboundId LDAPConnection class and
+ * performs the startTLS extended operation.
  *
  * @author  Middleware Services
  * @version  $Revision$
  */
-public class UnboundIdConnectionFactory
+public class UnboundIdStartTLSConnectionFactory
   extends AbstractConnectionFactory<UnboundIdProviderConfig>
 {
 
-  /** Socket factory to use for LDAP and LDAPS connections. */
+  /** Socket factory to use for connections. */
   private final SocketFactory socketFactory;
+
+  /** SSL context to use for startTLS. */
+  private final SSLContext sslContext;
 
   /** Unboundid connection options. */
   private final LDAPConnectionOptions ldapOptions;
@@ -44,15 +52,18 @@ public class UnboundIdConnectionFactory
    *
    * @param  url  of the ldap to connect to
    * @param  factory  SSL socket factory to use for LDAP and LDAPS
+   * @param  sslCtx  SSL context to use for startTLS
    * @param  options  connection options
    */
-  public UnboundIdConnectionFactory(
+  public UnboundIdStartTLSConnectionFactory(
     final String url,
     final SocketFactory factory,
+    final SSLContext sslCtx,
     final LDAPConnectionOptions options)
   {
     super(url);
     socketFactory = factory;
+    sslContext = sslCtx;
     ldapOptions = options;
   }
 
@@ -76,6 +87,15 @@ public class UnboundIdConnectionFactory
       conn.setSearchIgnoreResultCodes(
         getProviderConfig().getSearchIgnoreResultCodes());
       conn.setControlProcessor(getProviderConfig().getControlProcessor());
+
+      final ExtendedResult result = lc.processExtendedOperation(
+        new StartTLSExtendedRequest(sslContext));
+      if (result.getResultCode() != ResultCode.SUCCESS) {
+        closeConn = true;
+        throw new ConnectionException(
+          "StartTLS failed",
+          org.ldaptive.ResultCode.valueOf(result.getResultCode().intValue()));
+      }
     } catch (LDAPException e) {
       closeConn = true;
       throw new ConnectionException(
