893bcdedc3f297d5c6139125cfca7874f8cda592
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2012-08-09 19:38:46 +0000

    Tagging RC5 release.

---
+++ ldaptive/unboundid-provider/tags/ldaptive-unboundid-1.0-RC5/src/main/java/org/ldaptive/provider/unboundid/UnboundIDConnectionFactory.java
@@ -0,0 +1,94 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2012 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.provider.unboundid;
+
+import javax.net.SocketFactory;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPConnectionOptions;
+import com.unboundid.ldap.sdk.LDAPException;
+import org.ldaptive.LdapException;
+import org.ldaptive.LdapURL;
+import org.ldaptive.provider.AbstractProviderConnectionFactory;
+import org.ldaptive.provider.ConnectionException;
+
+/**
+ * Creates ldap connections using the UnboundID LDAPConnection class.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class UnboundIDConnectionFactory
+  extends AbstractProviderConnectionFactory<UnboundIDProviderConfig>
+{
+
+  /** Socket factory to use for LDAP and LDAPS connections. */
+  private final SocketFactory socketFactory;
+
+  /** Unboundid connection options. */
+  private final LDAPConnectionOptions ldapOptions;
+
+
+  /**
+   * Creates a new Unbound ID connection factory.
+   *
+   * @param  url  of the ldap to connect to
+   * @param  config  provider configuration
+   * @param  factory  SSL socket factory to use for LDAP and LDAPS
+   * @param  options  connection options
+   */
+  public UnboundIDConnectionFactory(
+    final String url,
+    final UnboundIDProviderConfig config,
+    final SocketFactory factory,
+    final LDAPConnectionOptions options)
+  {
+    super(url, config);
+    socketFactory = factory;
+    ldapOptions = options;
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  protected UnboundIDConnection createInternal(final String url)
+    throws LdapException
+  {
+    final LdapURL ldapUrl = new LdapURL(url);
+    UnboundIDConnection conn = null;
+    boolean closeConn = false;
+    try {
+      final LDAPConnection lc = new LDAPConnection(socketFactory, ldapOptions);
+      conn = new UnboundIDConnection(lc, getProviderConfig());
+      lc.connect(
+        ldapUrl.getLastEntry().getHostname(),
+        ldapUrl.getLastEntry().getPort());
+    } catch (LDAPException e) {
+      closeConn = true;
+      throw new ConnectionException(
+        e,
+        org.ldaptive.ResultCode.valueOf(e.getResultCode().intValue()));
+    } finally {
+      if (closeConn) {
+        try {
+          if (conn != null) {
+            conn.close();
+          }
+        } catch (LdapException e) {
+          logger.debug("Problem tearing down connection", e);
+        }
+      }
+    }
+    return conn;
+  }
+}
---
+++ ldaptive/unboundid-provider/tags/ldaptive-unboundid-1.0-RC5/src/main/java/org/ldaptive/provider/unboundid/UnboundIDProvider.java
@@ -0,0 +1,195 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2012 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.provider.unboundid;
+
+import java.security.GeneralSecurityException;
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
+import com.unboundid.ldap.sdk.LDAPConnectionOptions;
+import org.ldaptive.ConnectionConfig;
+import org.ldaptive.LdapURL;
+import org.ldaptive.provider.Provider;
+import org.ldaptive.provider.ProviderConnectionFactory;
+import org.ldaptive.ssl.CredentialConfig;
+import org.ldaptive.ssl.DefaultHostnameVerifier;
+import org.ldaptive.ssl.DefaultSSLContextInitializer;
+import org.ldaptive.ssl.HostnameVerifyingTrustManager;
+import org.ldaptive.ssl.SSLContextInitializer;
+import org.ldaptive.ssl.TLSSocketFactory;
+
+/**
+ * UnboundID provider implementation. Provides connection factories for clear,
+ * SSL, and TLS connections.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class UnboundIDProvider implements Provider<UnboundIDProviderConfig>
+{
+
+  /** Provider configuration. */
+  private UnboundIDProviderConfig config = new UnboundIDProviderConfig();
+
+
+  /** {@inheritDoc} */
+  @Override
+  public ProviderConnectionFactory<UnboundIDProviderConfig>
+  getConnectionFactory(final ConnectionConfig cc)
+  {
+    SocketFactory factory = config.getSocketFactory();
+    SSLContext sslContext = null;
+    // Unboundid does not do hostname verification by default
+    // set a default hostname verifier if no trust settings have been configured
+    if (cc.getUseStartTLS()) {
+      sslContext = getHostnameVerifierSSLContext(cc);
+    } else if (cc.getUseSSL() && factory == null) {
+      factory = getHostnameVerifierSocketFactory(cc);
+    }
+    if (cc.getSslConfig() != null &&
+        cc.getSslConfig().getEnabledCipherSuites() != null) {
+      throw new UnsupportedOperationException(
+        "UnboundID provider does not support the cipher suites property");
+    }
+    if (cc.getSslConfig() != null &&
+        cc.getSslConfig().getEnabledProtocols() != null) {
+      throw new UnsupportedOperationException(
+        "UnboundID provider does not support the protocols property");
+    }
+
+    LDAPConnectionOptions options = config.getConnectionOptions();
+    if (options == null) {
+      options = getDefaultLDAPConnectionOptions(cc);
+    }
+
+    ProviderConnectionFactory<UnboundIDProviderConfig> cf;
+    if (cc.getUseStartTLS()) {
+      cf = new UnboundIDStartTLSConnectionFactory(
+        cc.getLdapUrl(),
+        config,
+        factory,
+        sslContext,
+        options);
+    } else {
+      cf = new UnboundIDConnectionFactory(
+        cc.getLdapUrl(),
+        config,
+        factory,
+        options);
+    }
+    return cf;
+  }
+
+
+  /**
+   * Returns an SSLContext configured with a default hostname verifier. Uses a
+   * {@link DefaultHostnameVerifier} if no trust managers have been configured.
+   *
+   * @param  cc  connection configuration
+   *
+   * @return  SSL Context
+   */
+  protected SSLContext getHostnameVerifierSSLContext(final ConnectionConfig cc)
+  {
+    SSLContext sslContext;
+    SSLContextInitializer contextInit;
+    if (cc.getSslConfig() != null &&
+        cc.getSslConfig().getCredentialConfig() != null) {
+      try {
+        final CredentialConfig credConfig =
+          cc.getSslConfig().getCredentialConfig();
+        contextInit = credConfig.createSSLContextInitializer();
+      } catch (GeneralSecurityException e) {
+        throw new IllegalArgumentException(e);
+      }
+    } else {
+      contextInit = new DefaultSSLContextInitializer();
+    }
+    if (cc.getSslConfig() != null &&
+        cc.getSslConfig().getTrustManagers() != null) {
+      contextInit.setTrustManagers(cc.getSslConfig().getTrustManagers());
+    } else {
+      final LdapURL ldapUrl = new LdapURL(cc.getLdapUrl());
+      contextInit.setTrustManagers(
+        new HostnameVerifyingTrustManager(
+          new DefaultHostnameVerifier(),
+          ldapUrl.getEntriesAsString()));
+    }
+    try {
+      sslContext = contextInit.initSSLContext("TLS");
+    } catch (GeneralSecurityException e) {
+      throw new IllegalArgumentException(e);
+    }
+    return sslContext;
+  }
+
+
+  /**
+   * Returns an SSL socket factory configured with a default hostname verifier.
+   *
+   * @param  cc  connection configuration
+   *
+   * @return  SSL socket factory
+   */
+  protected SocketFactory getHostnameVerifierSocketFactory(
+    final ConnectionConfig cc)
+  {
+    final LdapURL ldapUrl = new LdapURL(cc.getLdapUrl());
+    return
+      TLSSocketFactory.getHostnameVerifierFactory(
+        cc.getSslConfig(),
+        ldapUrl.getEntriesAsString());
+  }
+
+
+  /**
+   * Returns the default connection options for this provider.
+   *
+   * @param  cc  to configure options with
+   *
+   * @return  ldap connection options
+   */
+  protected LDAPConnectionOptions getDefaultLDAPConnectionOptions(
+    final ConnectionConfig cc)
+  {
+    final LDAPConnectionOptions options = new LDAPConnectionOptions();
+    options.setConnectTimeoutMillis(
+      cc.getConnectTimeout() > 0 ? (int) cc.getConnectTimeout() : 0);
+    options.setResponseTimeoutMillis(cc.getResponseTimeout());
+    return options;
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public UnboundIDProviderConfig getProviderConfig()
+  {
+    return config;
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public void setProviderConfig(final UnboundIDProviderConfig jpc)
+  {
+    config = jpc;
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public UnboundIDProvider newInstance()
+  {
+    return new UnboundIDProvider();
+  }
+}
