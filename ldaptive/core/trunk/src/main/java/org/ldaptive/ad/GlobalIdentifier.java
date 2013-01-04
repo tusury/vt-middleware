de4030cf5f11a2cae2ba3b7ccc0cef41e7cce232
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2013-01-04 18:41:36 +0000

    Add support for handling the active directory attributes objectSid and objectGuid.

---
+++ ldaptive/core/trunk/src/main/java/org/ldaptive/ad/GlobalIdentifier.java
@@ -0,0 +1,170 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2013 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.ad;
+
+import java.nio.ByteBuffer;
+import java.util.StringTokenizer;
+import org.ldaptive.LdapUtils;
+
+/**
+ * Class to represent an active directory GUID. Provides conversion from binary
+ * to string and vice versa.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public final class GlobalIdentifier
+{
+
+
+  /** Default constructor. */
+  private GlobalIdentifier() {}
+
+
+  /**
+   * Converts the supplied GUID to it's string format.
+   *
+   * @param  guid  to convert
+   *
+   * @return  string format of the GUID
+   */
+  public static String toString(final byte[] guid)
+  {
+    // CheckStyle:MagicNumber OFF
+
+    // create a byte buffer for reading the guid
+    final ByteBuffer guidBuffer = ByteBuffer.wrap(guid);
+
+    // string identifier
+    final StringBuilder sb = new StringBuilder("{");
+    // encode the first 4 bytes, big endian
+    guidBuffer.limit(4);
+    sb.append(LdapUtils.hexEncode(getBytes(guidBuffer, true)));
+
+    // encode the next 2 bytes, big endian
+    guidBuffer.limit(6);
+    sb.append("-").append(LdapUtils.hexEncode(getBytes(guidBuffer, true)));
+
+    // encode the next 2 bytes, big endian
+    guidBuffer.limit(8);
+    sb.append("-").append(LdapUtils.hexEncode(getBytes(guidBuffer, true)));
+
+    // encode the next 2 bytes, little endian
+    guidBuffer.limit(10);
+    sb.append("-").append(LdapUtils.hexEncode(getBytes(guidBuffer, false)));
+
+    // encode the last 6 bytes, little endian
+    guidBuffer.limit(guidBuffer.capacity());
+    sb.append("-").append(LdapUtils.hexEncode(getBytes(guidBuffer, false)));
+    sb.append("}");
+
+    return sb.toString();
+    // CheckStyle:MagicNumber ON
+  }
+
+
+  /**
+   * Converts the supplied GUID to it's binary format.
+   *
+   * @param  guid  to convert
+   *
+   * @return  binary format of the GUID
+   */
+  public static byte[] toBytes(final String guid)
+  {
+    // CheckStyle:MagicNumber OFF
+
+    // remove the enclosing brackets {...}
+    final StringTokenizer st = new StringTokenizer(
+      guid.substring(1, guid.length() - 1), "-");
+    // first token is 4 bytes, big endian
+    final String data1 = st.nextToken();
+    // second token is 2 bytes, big endian
+    final String data2 = st.nextToken();
+    // third token is 2 bytes, big endian
+    final String data3 = st.nextToken();
+    // fourth token is 2 bytes, little endian
+    final String data4 = st.nextToken();
+    // fifth token is 6 bytes, little endian
+    final String data5 = st.nextToken();
+
+    final ByteBuffer guidBuffer = ByteBuffer.allocate(16);
+    putBytes(guidBuffer, LdapUtils.hexDecode(data1.toCharArray()), true);
+    putBytes(guidBuffer, LdapUtils.hexDecode(data2.toCharArray()), true);
+    putBytes(guidBuffer, LdapUtils.hexDecode(data3.toCharArray()), true);
+    putBytes(guidBuffer, LdapUtils.hexDecode(data4.toCharArray()), false);
+    putBytes(guidBuffer, LdapUtils.hexDecode(data5.toCharArray()), false);
+
+    return guidBuffer.array();
+    // CheckStyle:MagicNumber ON
+  }
+
+
+  /**
+   * Reads bytes from the supplied byte buffer. The byte buffer limit must be
+   * set appropriately by the caller.
+   *
+   * @param  buffer  to read bytes from
+   * @param  bigEndian  whether to return the bytes as big endian
+   *
+   * @return  long value
+   */
+  private static byte[] getBytes(
+    final ByteBuffer buffer,
+    final boolean bigEndian)
+  {
+    // CheckStyle:MagicNumber OFF
+    final byte[] bytes = new byte[buffer.limit() - buffer.position()];
+    if (bigEndian) {
+      int offset = bytes.length - 1;
+      while (buffer.hasRemaining()) {
+        bytes[offset--] = (byte) (buffer.get() & 0xFF);
+      }
+    } else {
+      int offset = 0;
+      while (buffer.hasRemaining()) {
+        bytes[offset++] = (byte) (buffer.get() & 0xFF);
+      }
+    }
+    return bytes;
+    // CheckStyle:MagicNumber ON
+  }
+
+
+  /**
+   * Writes a long into the supplied byte buffer. The byte buffer limit must be
+   * set appropriately by the caller.
+   *
+   * @param  buffer  to write long to
+   * @param  bytes  to write
+   * @param  bigEndian  whether to write the bytes as big endian
+   */
+  private static void putBytes(
+    final ByteBuffer buffer,
+    final byte[] bytes,
+    final boolean bigEndian)
+  {
+    // CheckStyle:MagicNumber OFF
+    if (bigEndian) {
+      for (int i = bytes.length - 1; i >= 0; i--) {
+        buffer.put((byte) (bytes[i] & 0xFF));
+      }
+    } else {
+      for (byte b : bytes) {
+        buffer.put((byte) (b & 0xFF));
+      }
+    }
+    // CheckStyle:MagicNumber ON
+  }
+}
---
+++ ldaptive/core/trunk/src/test/java/org/ldaptive/ad/GlobalIdentifierTest.java
@@ -0,0 +1,85 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2013 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.ad;
+
+import org.ldaptive.LdapUtils;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for {@link GlobalIdentifier}.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class GlobalIdentifierTest
+{
+
+
+  /**
+   * ObjectGuid test data.
+   *
+   * @return  test data
+   *
+   * @throws  Exception  if test data cannot be generated
+   */
+  @DataProvider(name = "guids")
+  public Object[][] createGuids()
+    throws Exception
+  {
+    return new Object[][] {
+      new Object[] {
+        "{B1DB3CCA-72BD-4F31-9EBF-C70CD44BDA32}",
+        LdapUtils.base64Decode("yjzbsb1yMU+ev8cM1EvaMg=="),
+      },
+      new Object[] {
+        "{0F0BF778-1C43-4D0C-82E6-BAD22D6AB646}",
+        LdapUtils.base64Decode("ePcLD0McDE2C5rrSLWq2Rg=="),
+      },
+      new Object[] {
+        "{36B403E2-BA7F-4A83-8049-B3CD202C7032}",
+        LdapUtils.base64Decode("4gO0Nn+6g0qASbPNICxwMg=="),
+      },
+    };
+  }
+
+
+  /**
+   * @param  guidString  objectGuid string form
+   * @param  guid  global identifier
+   *
+   * @throws  Exception  On test failure.
+   */
+  @Test(groups = {"ad"}, dataProvider = "guids")
+  public void testToString(final String guidString, final byte[] guid)
+    throws Exception
+  {
+    Assert.assertEquals(GlobalIdentifier.toString(guid), guidString);
+  }
+
+
+  /**
+   * @param  guidString  objectGuid string form
+   * @param  guid  global identifier
+   *
+   * @throws  Exception  On test failure.
+   */
+  @Test(groups = {"ad"}, dataProvider = "guids")
+  public void testToBytes(final String guidString, final byte[] guid)
+    throws Exception
+  {
+    Assert.assertEquals(guid, GlobalIdentifier.toBytes(guidString));
+  }
+}
