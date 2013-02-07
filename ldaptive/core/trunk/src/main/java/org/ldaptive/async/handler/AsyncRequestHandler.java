a51aa79542c45b08465ad6664488e8afad4efc19
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2013-02-07 17:49:20 +0000

    Set svn keyword properties.

--- ldaptive/core/trunk/src/main/java/org/ldaptive/async/handler/AsyncRequestHandler.java
+++ ldaptive/core/trunk/src/main/java/org/ldaptive/async/handler/AsyncRequestHandler.java
@@ -1,5 +1,5 @@
 /*
-  $Id: AsyncRequestHandler.java 2593 2013-01-29 20:21:24Z dfisher $
+  $Id$
 
   Copyright (C) 2003-2013 Virginia Tech.
   All rights reserved.
@@ -8,8 +8,8 @@
 
   Author:  Middleware Services
   Email:   middleware@vt.edu
-  Version: $Revision: 2593 $
-  Updated: $Date: 2013-01-29 15:21:24 -0500 (Tue, 29 Jan 2013) $
+  Version: $Revision$
+  Updated: $Date$
 */
 package org.ldaptive.async.handler;
 
@@ -24,7 +24,7 @@ import org.ldaptive.handler.HandlerResult;
  * Provides post processing of an ldap async request.
  *
  * @author  Middleware Services
- * @version  $Revision: 2593 $ $Date: 2013-01-29 15:21:24 -0500 (Tue, 29 Jan 2013) $
+ * @version  $Revision$ $Date$
  */
 public interface AsyncRequestHandler extends Handler<Request, AsyncRequest>
 {
