875464e1e1de02a78c61752d9ed86d98e4fe2478
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2013-02-21 23:16:29 +0000

    Tagging release RC8.

---
+++ ldaptive/opendj-provider/tags/ldaptive-opendj-1.0-RC8/src/main/java/org/ldaptive/provider/opendj/OpenDJControlHandler.java
@@ -0,0 +1,76 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2013 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.provider.opendj;
+
+import org.forgerock.opendj.ldap.ByteStringBuilder;
+import org.forgerock.opendj.ldap.controls.GenericControl;
+import org.ldaptive.control.ControlFactory;
+import org.ldaptive.control.RequestControl;
+import org.ldaptive.control.ResponseControl;
+import org.ldaptive.provider.ControlHandler;
+
+/**
+ * OpenDJ control handler.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class OpenDJControlHandler
+  implements ControlHandler<org.forgerock.opendj.ldap.controls.Control>
+{
+
+
+  /** {@inheritDoc} */
+  @Override
+  public String getOID(final org.forgerock.opendj.ldap.controls.Control control)
+  {
+    return control.getOID();
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public org.forgerock.opendj.ldap.controls.Control handleRequest(
+    final RequestControl requestControl)
+  {
+    final byte[] value = requestControl.encode();
+    if (value == null) {
+      return
+        GenericControl.newControl(
+          requestControl.getOID(),
+          requestControl.getCriticality());
+    } else {
+      final ByteStringBuilder builder = new ByteStringBuilder(value.length);
+      builder.append(value);
+      return
+        GenericControl.newControl(
+          requestControl.getOID(),
+          requestControl.getCriticality(),
+          builder.toByteString());
+    }
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseControl handleResponse(
+    final org.forgerock.opendj.ldap.controls.Control responseControl)
+  {
+    return
+      ControlFactory.createResponseControl(
+        responseControl.getOID(),
+        responseControl.isCritical(),
+        responseControl.getValue().toByteArray());
+  }
+}
