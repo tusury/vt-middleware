a81517f38f8b83e98b5f9206f059fc925c4a2332
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2013-02-21 23:17:39 +0000

    Tagging release RC8.

---
+++ ldaptive/templates/tags/ldaptive-templates-1.0-RC8/src/main/java/org/ldaptive/servlets/Dsmlv1ServletSearchTemplatesExecutor.java
@@ -0,0 +1,48 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2013 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.servlets;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import javax.servlet.http.HttpServletResponse;
+import org.ldaptive.SearchResult;
+import org.ldaptive.io.Dsmlv1Writer;
+
+/**
+ * Writes search results as DSML version 1. See {@link
+ * AbstractServletSearchTemplatesExecutor}.
+ *
+ * @author  Middleware Services
+ * @version  $Revision $ $Date$
+ */
+public class Dsmlv1ServletSearchTemplatesExecutor
+  extends AbstractServletSearchTemplatesExecutor
+{
+
+
+  /** {@inheritDoc} */
+  @Override
+  protected void writeResponse(
+    final SearchResult result,
+    final HttpServletResponse response)
+    throws IOException
+  {
+    response.setContentType("text/xml");
+
+    final Dsmlv1Writer writer = new Dsmlv1Writer(
+      new BufferedWriter(new OutputStreamWriter(response.getOutputStream())));
+    writer.write(result);
+  }
+}
