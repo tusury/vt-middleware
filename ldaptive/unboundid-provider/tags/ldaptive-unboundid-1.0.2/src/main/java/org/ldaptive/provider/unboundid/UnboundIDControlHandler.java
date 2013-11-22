5baffb85d4c7cdb3697611c6e39dab113c33a4f3
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2013-11-22 19:44:46 +0000

    Tagging version 1.0.2

---
+++ ldaptive/unboundid-provider/tags/ldaptive-unboundid-1.0.2/src/main/java/org/ldaptive/provider/unboundid/UnboundIDControlHandler.java
@@ -0,0 +1,73 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2013 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.provider.unboundid;
+
+import com.unboundid.asn1.ASN1OctetString;
+import org.ldaptive.control.ControlFactory;
+import org.ldaptive.control.RequestControl;
+import org.ldaptive.control.ResponseControl;
+import org.ldaptive.provider.ControlHandler;
+
+/**
+ * Unbound ID control handler.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class UnboundIDControlHandler
+  implements ControlHandler<com.unboundid.ldap.sdk.Control>
+{
+
+
+  /** {@inheritDoc} */
+  @Override
+  public String getOID(final com.unboundid.ldap.sdk.Control control)
+  {
+    return control.getOID();
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public com.unboundid.ldap.sdk.Control handleRequest(
+    final RequestControl requestControl)
+  {
+    final byte[] value = requestControl.encode();
+    if (value == null) {
+      return
+        new com.unboundid.ldap.sdk.Control(
+          requestControl.getOID(),
+          requestControl.getCriticality());
+    } else {
+      return
+        new com.unboundid.ldap.sdk.Control(
+          requestControl.getOID(),
+          requestControl.getCriticality(),
+          new ASN1OctetString(value));
+    }
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseControl handleResponse(
+    final com.unboundid.ldap.sdk.Control responseControl)
+  {
+    return
+      ControlFactory.createResponseControl(
+        responseControl.getOID(),
+        responseControl.isCritical(),
+        responseControl.getValue().getValue());
+  }
+}
