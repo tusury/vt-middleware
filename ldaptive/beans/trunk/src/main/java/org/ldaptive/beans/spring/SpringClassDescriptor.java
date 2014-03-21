4d9be9659451abeda64ed0f1b1a131e8ec9f26ed
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2014-03-21 20:09:25 +0000

    Add support for setting custom converters on SpringLdapEntryMapper.

--- ldaptive/beans/trunk/src/main/java/org/ldaptive/beans/spring/SpringClassDescriptor.java
+++ ldaptive/beans/trunk/src/main/java/org/ldaptive/beans/spring/SpringClassDescriptor.java
@@ -13,27 +13,28 @@
 */
 package org.ldaptive.beans.spring;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import org.ldaptive.SortBehavior;
 import org.ldaptive.beans.AbstractClassDescriptor;
 import org.ldaptive.beans.Attribute;
 import org.ldaptive.beans.AttributeValueMutator;
 import org.ldaptive.beans.DnValueMutator;
 import org.ldaptive.beans.Entry;
-import org.ldaptive.io.GeneralizedTimeValueTranscoder;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.convert.support.DefaultConversionService;
-import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.SpelParseException;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
-import org.springframework.expression.spel.support.StandardTypeConverter;
 
 /**
  * Spring implementation of a class descriptor. Uses an {@link
@@ -46,56 +47,17 @@ public class SpringClassDescriptor extends AbstractClassDescriptor
 {
 
   /** Context for evaluating spring expressions. */
-  private final StandardEvaluationContext context;
-
-  /** Conversion service for object conversion. */
-  private final GenericConversionService conversionService;
+  private final EvaluationContext evaluationContext;
 
 
   /**
    * Creates a new spring class descriptor.
    *
-   * @param  object  to describe
-   * @param  converters  to add to the conversion service
+   * @param  context  to use for SPEL evaluation
    */
-  public SpringClassDescriptor(
-    final Object object,
-    final Converter<?, ?>... converters)
+  public SpringClassDescriptor(final EvaluationContext context)
   {
-    conversionService = new GenericConversionService();
-    DefaultConversionService.addDefaultConverters(conversionService);
-    if (converters != null) {
-      for (Converter<?, ?> converter : converters) {
-        conversionService.addConverter(converter);
-      }
-    }
-    if (!conversionService.canConvert(String.class, Calendar.class)) {
-      conversionService.addConverter(new Converter<String, Calendar>()
-      {
-        @Override
-        public Calendar convert(final String s)
-        {
-          final GeneralizedTimeValueTranscoder transcoder = new
-            GeneralizedTimeValueTranscoder();
-          return transcoder.decodeStringValue(s);
-        }
-      });
-    }
-    if (!conversionService.canConvert(Calendar.class, String.class)) {
-      conversionService.addConverter(new Converter<Calendar, String>()
-      {
-        @Override
-        public String convert(final Calendar c)
-        {
-          final GeneralizedTimeValueTranscoder transcoder = new
-            GeneralizedTimeValueTranscoder();
-          return transcoder.encodeStringValue(c);
-        }
-      });
-    }
-
-    context = new StandardEvaluationContext(object);
-    context.setTypeConverter(new StandardTypeConverter(conversionService));
+    evaluationContext = context;
   }
 
 
@@ -115,13 +77,13 @@ public class SpringClassDescriptor extends AbstractClassDescriptor
             @Override
             public String getValue(final Object object)
             {
-              return exp.getValue(context, object, String.class);
+              return exp.getValue(evaluationContext, object, String.class);
             }
 
             @Override
             public void setValue(final Object object, final String value)
             {
-              exp.setValue(context, object, value);
+              exp.setValue(evaluationContext, object, value);
             }
           });
         } catch (SpelParseException e) {
@@ -168,56 +130,112 @@ public class SpringClassDescriptor extends AbstractClassDescriptor
             @Override
             public Collection<String> getStringValues(final Object object)
             {
-              final Object converted = exp.getValue(context, object);
-              if (Collection.class.isAssignableFrom(converted.getClass())) {
-                final Collection<?> c = (Collection<?>) converted;
-                final List<String> l = new ArrayList<String>(c.size());
-                for (Object o : c) {
-                  l.add(conversionService.convert(o, String.class));
-                }
-                return l;
-              } else {
-                final List<String> l = new ArrayList<String>(1);
-                l.add(conversionService.convert(converted, String.class));
-                return l;
-              }
+              return getValues(object, String.class);
             }
 
             @Override
             public Collection<byte[]> getBinaryValues(final Object object)
             {
-              final Object converted = exp.getValue(context, object);
-              if (Collection.class.isAssignableFrom(converted.getClass())) {
-                final Collection<?> c = (Collection<?>) converted;
-                final List<byte[]> l = new ArrayList<byte[]>(c.size());
-                for (Object o : c) {
-                  l.add(conversionService.convert(o, byte[].class));
+              return getValues(object, byte[].class);
+            }
+
+            @SuppressWarnings("unchecked")
+            protected <T> Collection<T> getValues(
+              final Object object,
+              final Class<T> type)
+            {
+              Collection<T> values = null;
+              final Object converted = exp.getValue(evaluationContext, object);
+              if (converted != null) {
+                if (converted.getClass().isArray()) {
+                  final int length = Array.getLength(converted);
+                  values = createCollection(List.class, length);
+                  for (int i = 0; i < length; i++) {
+                    final Object o = Array.get(converted, i);
+                    values.add(
+                      (T) evaluationContext.getTypeConverter().convertValue(
+                        o,
+                        TypeDescriptor.valueOf(o.getClass()),
+                        TypeDescriptor.valueOf(type)));
+                  }
+                } else if (Collection.class.isAssignableFrom(
+                           converted.getClass())) {
+                  final Collection<?> col = (Collection<?>) converted;
+                  values = createCollection(converted.getClass(), col.size());
+                  for (Object o : col) {
+                    values.add(
+                      (T) evaluationContext.getTypeConverter().convertValue(
+                        o,
+                        TypeDescriptor.valueOf(o.getClass()),
+                        TypeDescriptor.valueOf(type)));
+                  }
+                } else {
+                  values = createCollection(List.class, 1);
+                  values.add(
+                    (T) evaluationContext.getTypeConverter().convertValue(
+                      converted,
+                      TypeDescriptor.valueOf(converted.getClass()),
+                      TypeDescriptor.valueOf(type)));
                 }
-                return l;
-              } else {
-                final List<byte[]> l = new ArrayList<byte[]>(1);
-                l.add(conversionService.convert(converted, byte[].class));
-                return l;
               }
+              return values;
             }
 
             @Override
             public void setStringValues(
               final Object object, final Collection<String> values)
             {
-              exp.setValue(context, object, values);
+              exp.setValue(evaluationContext, object, values);
             }
 
             @Override
             public void setBinaryValues(
               final Object object, final Collection<byte[]> values)
             {
-              exp.setValue(context, object, values);
+              exp.setValue(evaluationContext, object, values);
             }
           });
           // CheckStyle:AnonInnerLength ON
         }
       }
+    }
+  }
+
+
+  /**
+   * Creates a best fit collection for the supplied type.
+   *
+   * @param  <T>  collection type
+   * @param  type  of collection to create
+   * @param  size  of the collection
+   *
+   * @return  collection
+   */
+  protected <T> Collection<T> createCollection(
+    final Class<?> type,
+    final int size)
+  {
+    if (type == Collection.class || List.class.isAssignableFrom(type)) {
+      List<T> l;
+      if (LinkedList.class.isAssignableFrom(type)) {
+        l = new LinkedList<T>();
+      } else {
+        l = new ArrayList<T>(size);
+      }
+      return l;
+    } else if (Set.class.isAssignableFrom(type)) {
+      Set<T> s;
+      if (LinkedHashSet.class.isAssignableFrom(type)) {
+        s = new LinkedHashSet<T>(size);
+      } else if (TreeSet.class.isAssignableFrom(type)) {
+        s = new TreeSet<T>();
+      } else {
+        s = new HashSet<T>(size);
+      }
+      return s;
+    } else {
+      throw new IllegalArgumentException(
+        "Unsupported collection type: " + type);
     }
   }
 }
