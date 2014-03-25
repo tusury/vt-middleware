487f799e44e2eef6ac765e2fc8d71368431b1957
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2014-03-25 18:31:48 +0000

    Break out inner classes.

---
+++ ldaptive/beans/trunk/src/main/java/org/ldaptive/beans/spring/SpelAttributeValueMutator.java
@@ -0,0 +1,245 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2014 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.beans.spring;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import org.ldaptive.SortBehavior;
+import org.ldaptive.beans.Attribute;
+import org.ldaptive.beans.AttributeValueMutator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+/**
+ * Attribute mutator that uses a SPEL expression and evaluation context.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class SpelAttributeValueMutator implements AttributeValueMutator
+{
+
+  /** Logger for this class. */
+  protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+  /** Attribute containing the SPEL expression. */
+  private final Attribute attribute;
+
+  /** SPEL expression to access values. */
+  private final Expression expression;
+
+  /** Evaluation context. */
+  private final EvaluationContext evaluationContext;
+
+
+  /**
+   * Creates a new spel attribute value mutator.
+   *
+   * @param  attr  containing the SPEL configuration
+   * @param  context  containing the values
+   */
+  public SpelAttributeValueMutator(
+    final Attribute attr,
+    final EvaluationContext context)
+  {
+    attribute = attr;
+    final ExpressionParser parser = new SpelExpressionParser();
+    expression = parser.parseExpression(
+      attribute.property().length() > 0 ?
+        attribute.property() : attribute.name());
+    evaluationContext = context;
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public String getName()
+  {
+    return attribute.name();
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public boolean isBinary()
+  {
+    return attribute.binary();
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public SortBehavior getSortBehavior()
+  {
+    return attribute.sortBehavior();
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public Collection<String> getStringValues(final Object object)
+  {
+    return getValues(object, String.class);
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public Collection<byte[]> getBinaryValues(final Object object)
+  {
+    return getValues(object, byte[].class);
+  }
+
+
+  /**
+   * Uses the configured expression and evaluation context to retrieve values
+   * from the supplied object. Values are the placed in a collection and
+   * returned.
+   *
+   * @param  <T>  either String or byte[]
+   * @param  object  to get values from
+   * @param  type  of objects to place in the collection
+   *
+   * @return  values in the supplied object
+   */
+  @SuppressWarnings("unchecked")
+  protected <T> Collection<T> getValues(
+    final Object object,
+    final Class<T> type)
+  {
+    Collection<T> values = null;
+    final Object converted = expression.getValue(evaluationContext, object);
+    if (converted != null) {
+      if (converted.getClass().isArray()) {
+        final int length = Array.getLength(converted);
+        values = createCollection(List.class, length);
+        for (int i = 0; i < length; i++) {
+          final Object o = Array.get(converted, i);
+          final T value =
+            (T) evaluationContext.getTypeConverter().convertValue(
+              o,
+              TypeDescriptor.valueOf(o.getClass()),
+              TypeDescriptor.valueOf(type));
+          if (value != null) {
+            values.add(value);
+          }
+        }
+      } else if (Collection.class.isAssignableFrom(
+        converted.getClass())) {
+        final Collection<?> col = (Collection<?>) converted;
+        values = createCollection(converted.getClass(), col.size());
+        for (Object o : col) {
+          final T value =
+            (T) evaluationContext.getTypeConverter().convertValue(
+              o,
+              TypeDescriptor.valueOf(o.getClass()),
+              TypeDescriptor.valueOf(type));
+          if (value != null) {
+            values.add(value);
+          }
+        }
+      } else {
+        values = createCollection(List.class, 1);
+        final T value =
+          (T) evaluationContext.getTypeConverter().convertValue(
+            converted,
+            TypeDescriptor.valueOf(converted.getClass()),
+            TypeDescriptor.valueOf(type));
+        if (value != null) {
+          values.add(value);
+        }
+      }
+    }
+    return values;
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public void setStringValues(
+    final Object object, final Collection<String> values)
+  {
+    expression.setValue(evaluationContext, object, values);
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public void setBinaryValues(
+    final Object object, final Collection<byte[]> values)
+  {
+    expression.setValue(evaluationContext, object, values);
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public String toString()
+  {
+    return String.format(
+      "[%s@%d::attribute=%s, expression=%s, evaluationContext=%s]",
+      getClass().getName(),
+      hashCode(),
+      attribute,
+      expression,
+      evaluationContext);
+  }
+
+
+  /**
+   * Creates a best fit collection for the supplied type.
+   *
+   * @param  <T>  collection type
+   * @param  type  of collection to create
+   * @param  size  of the collection
+   *
+   * @return  collection
+   */
+  protected static <T> Collection<T> createCollection(
+    final Class<?> type,
+    final int size)
+  {
+    Collection<T> c;
+    if (List.class.isAssignableFrom(type)) {
+      if (LinkedList.class.isAssignableFrom(type)) {
+        c = new LinkedList<T>();
+      } else {
+        c = new ArrayList<T>(size);
+      }
+    } else if (Set.class.isAssignableFrom(type)) {
+      if (LinkedHashSet.class.isAssignableFrom(type)) {
+        c = new LinkedHashSet<T>(size);
+      } else if (TreeSet.class.isAssignableFrom(type)) {
+        c = new TreeSet<T>();
+      } else {
+        c = new HashSet<T>(size);
+      }
+    } else {
+      c = new ArrayList<T>(size);
+    }
+    return c;
+  }
+}
