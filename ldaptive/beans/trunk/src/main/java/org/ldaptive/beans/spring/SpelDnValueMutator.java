13276e75312f708c32910660e7ae56e38160c79e
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2014-03-25 18:31:48 +0000

    Break out inner classes.

---
+++ ldaptive/beans/trunk/src/main/java/org/ldaptive/beans/spring/SpelDnValueMutator.java
@@ -0,0 +1,84 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2014 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.beans.spring;
+
+import org.ldaptive.beans.DnValueMutator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+
+/**
+ * Uses a SPEL expression and evaluation context to mutate the configured DN of
+ * an object.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class SpelDnValueMutator implements DnValueMutator
+{
+
+  /** Logger for this class. */
+  protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+  /** SPEL expression to access the DN. */
+  private final Expression expression;
+
+  /** Evaluation context. */
+  private final EvaluationContext evaluationContext;
+
+
+  /**
+   * Creates a new spel dn value mutator.
+   *
+   * @param  exp  to access the DN
+   * @param  context  containing the DN
+   */
+  public SpelDnValueMutator(
+    final Expression exp,
+    final EvaluationContext context)
+  {
+    expression = exp;
+    evaluationContext = context;
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public String getValue(final Object object)
+  {
+    return expression.getValue(evaluationContext, object, String.class);
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public void setValue(final Object object, final String value)
+  {
+    expression.setValue(evaluationContext, object, value);
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public String toString()
+  {
+    return String.format(
+      "[%s@%d::expression=%s, evaluationContext=%s]",
+      getClass().getName(),
+      hashCode(),
+      expression,
+      evaluationContext);
+  }
+}
--- ldaptive/beans/trunk/src/main/java/org/ldaptive/beans/spring/SpringClassDescriptor.java
+++ ldaptive/beans/trunk/src/main/java/org/ldaptive/beans/spring/SpringClassDescriptor.java
@@ -13,26 +13,12 @@
 */
 package org.ldaptive.beans.spring;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import org.ldaptive.SortBehavior;
 import org.ldaptive.beans.AbstractClassDescriptor;
 import org.ldaptive.beans.Attribute;
-import org.ldaptive.beans.AttributeValueMutator;
 import org.ldaptive.beans.DnValueMutator;
 import org.ldaptive.beans.Entry;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.expression.EvaluationContext;
-import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.SpelParseException;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
@@ -70,28 +56,7 @@ public class SpringClassDescriptor extends AbstractClassDescriptor
       type, Entry.class);
     if (entryAnnotation != null) {
       if (!"".equals(entryAnnotation.dn())) {
-        final ExpressionParser parser = new SpelExpressionParser();
-        try {
-          final Expression exp = parser.parseExpression(entryAnnotation.dn());
-          setDnValueMutator(new DnValueMutator() {
-            @Override
-            public String getValue(final Object object)
-            {
-              return exp.getValue(evaluationContext, object, String.class);
-            }
-
-            @Override
-            public void setValue(final Object object, final String value)
-            {
-              exp.setValue(evaluationContext, object, value);
-            }
-          });
-        } catch (SpelParseException e) {
-          logger.debug(
-            "Could not parse dn expression, using SimpleDnValueMutator",
-            e);
-          setDnValueMutator(new SimpleDnValueMutator(entryAnnotation.dn()));
-        }
+        setDnValueMutator(createDnValueMutator(entryAnnotation.dn()));
       }
       for (final Attribute attr : entryAnnotation.attributes()) {
         if ("".equals(attr.property()) && attr.values().length > 0) {
@@ -102,109 +67,8 @@ public class SpringClassDescriptor extends AbstractClassDescriptor
               attr.binary(),
               attr.sortBehavior()));
         } else {
-          final String expr = attr.property().length() > 0 ?
-            attr.property() : attr.name();
-          final ExpressionParser parser = new SpelExpressionParser();
-          final Expression exp = parser.parseExpression(expr);
-          // CheckStyle:AnonInnerLength OFF
-          addAttributeValueMutator(new AttributeValueMutator()
-          {
-            @Override
-            public String getName()
-            {
-              return attr.name();
-            }
-
-            @Override
-            public boolean isBinary()
-            {
-              return attr.binary();
-            }
-
-            @Override
-            public SortBehavior getSortBehavior()
-            {
-              return attr.sortBehavior();
-            }
-
-            @Override
-            public Collection<String> getStringValues(final Object object)
-            {
-              return getValues(object, String.class);
-            }
-
-            @Override
-            public Collection<byte[]> getBinaryValues(final Object object)
-            {
-              return getValues(object, byte[].class);
-            }
-
-            @SuppressWarnings("unchecked")
-            protected <T> Collection<T> getValues(
-              final Object object,
-              final Class<T> type)
-            {
-              Collection<T> values = null;
-              final Object converted = exp.getValue(evaluationContext, object);
-              if (converted != null) {
-                if (converted.getClass().isArray()) {
-                  final int length = Array.getLength(converted);
-                  values = createCollection(List.class, length);
-                  for (int i = 0; i < length; i++) {
-                    final Object o = Array.get(converted, i);
-                    final T value =
-                      (T) evaluationContext.getTypeConverter().convertValue(
-                        o,
-                        TypeDescriptor.valueOf(o.getClass()),
-                        TypeDescriptor.valueOf(type));
-                    if (value != null) {
-                      values.add(value);
-                    }
-                  }
-                } else if (Collection.class.isAssignableFrom(
-                           converted.getClass())) {
-                  final Collection<?> col = (Collection<?>) converted;
-                  values = createCollection(converted.getClass(), col.size());
-                  for (Object o : col) {
-                    final T value =
-                      (T) evaluationContext.getTypeConverter().convertValue(
-                        o,
-                        TypeDescriptor.valueOf(o.getClass()),
-                        TypeDescriptor.valueOf(type));
-                    if (value != null) {
-                      values.add(value);
-                    }
-                  }
-                } else {
-                  values = createCollection(List.class, 1);
-                  final T value =
-                    (T) evaluationContext.getTypeConverter().convertValue(
-                      converted,
-                      TypeDescriptor.valueOf(converted.getClass()),
-                      TypeDescriptor.valueOf(type));
-                  if (value != null) {
-                    values.add(value);
-                  }
-                }
-              }
-              return values;
-            }
-
-            @Override
-            public void setStringValues(
-              final Object object, final Collection<String> values)
-            {
-              exp.setValue(evaluationContext, object, values);
-            }
-
-            @Override
-            public void setBinaryValues(
-              final Object object, final Collection<byte[]> values)
-            {
-              exp.setValue(evaluationContext, object, values);
-            }
-          });
-          // CheckStyle:AnonInnerLength ON
+          addAttributeValueMutator(
+            new SpelAttributeValueMutator(attr, evaluationContext));
         }
       }
     }
@@ -212,36 +76,24 @@ public class SpringClassDescriptor extends AbstractClassDescriptor
 
 
   /**
-   * Creates a best fit collection for the supplied type.
+   * Creates a dn value mutator for the supplied SPEL dn property expression. If
+   * an expression cannot be created, a simple dn value mutator is returned.
    *
-   * @param  <T>  collection type
-   * @param  type  of collection to create
-   * @param  size  of the collection
+   * @param  dnProperty  SPEL expression
    *
-   * @return  collection
+   * @return  {@link SpelDnValueMutator} or {@link SimpleDnValueMutator}
    */
-  protected <T> Collection<T> createCollection(
-    final Class<?> type,
-    final int size)
+  protected DnValueMutator createDnValueMutator(final String dnProperty)
   {
-    Collection<T> c;
-    if (List.class.isAssignableFrom(type)) {
-      if (LinkedList.class.isAssignableFrom(type)) {
-        c = new LinkedList<T>();
-      } else {
-        c = new ArrayList<T>(size);
-      }
-    } else if (Set.class.isAssignableFrom(type)) {
-      if (LinkedHashSet.class.isAssignableFrom(type)) {
-        c = new LinkedHashSet<T>(size);
-      } else if (TreeSet.class.isAssignableFrom(type)) {
-        c = new TreeSet<T>();
-      } else {
-        c = new HashSet<T>(size);
-      }
-    } else {
-      c = new ArrayList<T>(size);
+    try {
+      return new SpelDnValueMutator(
+        new SpelExpressionParser().parseExpression(dnProperty),
+        evaluationContext);
+    } catch (SpelParseException e) {
+      logger.debug(
+        "Could not parse dn expression, using SimpleDnValueMutator",
+        e);
+      return new SimpleDnValueMutator(dnProperty);
     }
-    return c;
   }
 }
