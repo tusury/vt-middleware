49e661e2242cd1fefd8cc9c68cbcd40e6ec98cc0
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2014-03-26 14:10:52 +0000

    Add some tests for new beans package. Update dependencies.

---
+++ ldaptive/integration/src/test/java/org/ldaptive/beans/generate/BeanGeneratorTest.java
@@ -0,0 +1,88 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2010 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.beans.generate;
+
+import org.ldaptive.AbstractTest;
+import org.ldaptive.DefaultConnectionFactory;
+import org.ldaptive.TestControl;
+import org.ldaptive.TestUtils;
+import org.ldaptive.schema.Schema;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for {@link BeanGenerator}.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class BeanGeneratorTest extends AbstractTest
+{
+
+
+  /**
+   * @throws  Exception  On test failure.
+   */
+  @Test(groups = {"beans-generate"})
+  public void generate()
+    throws Exception
+  {
+    if (TestControl.isActiveDirectory()) {
+      return;
+    }
+    final DefaultConnectionFactory factory = new DefaultConnectionFactory(
+      TestUtils.readConnectionConfig(
+        "classpath:/org/ldaptive/ldap.setup.properties"));
+    final Schema schema = org.ldaptive.schema.SchemaFactory.createSchema(
+      factory);
+    final BeanGenerator generator = new BeanGenerator();
+    generator.setSchema(schema);
+    generator.setExcludedNames("objectClass");
+    generator.setObjectClasses("inetOrgPerson");
+    generator.setPackageName("org.ldaptive.beans.schema");
+    generator.setUseOperationalAttributes(true);
+    generator.setUseOptionalAttributes(true);
+    generator.setIncludeSuperiorClasses(true);
+    generator.generate();
+    generator.write("target/generated-test-sources/ldaptive");
+  }
+
+
+  /**
+   * @throws  Exception  On test failure.
+   */
+  @Test(groups = {"beans-generate"})
+  public void generateAD()
+    throws Exception
+  {
+    if (!TestControl.isActiveDirectory()) {
+      return;
+    }
+    final DefaultConnectionFactory factory = new DefaultConnectionFactory(
+      TestUtils.readConnectionConfig(
+        "classpath:/org/ldaptive/ldap.setup.properties"));
+    final Schema schema = org.ldaptive.ad.schema.SchemaFactory.createSchema(
+      factory,
+      "CN=Schema,CN=Configuration,DC=middleware,DC=vt,DC=edu");
+    final BeanGenerator generator = new BeanGenerator();
+    generator.setSchema(schema);
+    generator.setExcludedNames("objectClass");
+    generator.setObjectClasses("organizationalPerson");
+    generator.setPackageName("org.ldaptive.beans.schema");
+    generator.setUseOperationalAttributes(true);
+    generator.setUseOptionalAttributes(true);
+    generator.setIncludeSuperiorClasses(true);
+    generator.generate();
+    generator.write("target/generated-test-sources/ldaptive");
+  }
+}
