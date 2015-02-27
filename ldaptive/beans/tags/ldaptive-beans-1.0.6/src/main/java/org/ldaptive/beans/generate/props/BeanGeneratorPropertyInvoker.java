c81bb61d041b6b83b82760b7a57f1beaabc2a5b3
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2015-02-27 20:03:48 +0000

    Tagging version 1.0.6

---
+++ ldaptive/beans/tags/ldaptive-beans-1.0.6/src/main/java/org/ldaptive/beans/generate/props/BeanGeneratorPropertyInvoker.java
@@ -0,0 +1,74 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2014 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.beans.generate.props;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.ldaptive.props.AbstractPropertyInvoker;
+
+/**
+ * Handles properties for {@link org.ldaptive.beans.generate.BeanGenerator}.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class BeanGeneratorPropertyInvoker extends AbstractPropertyInvoker
+{
+
+
+  /**
+   * Creates a new bean generator property invoker for the supplied class.
+   *
+   * @param  c  class that has setter methods
+   */
+  public BeanGeneratorPropertyInvoker(final Class<?> c)
+  {
+    initialize(c);
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  @SuppressWarnings("unchecked")
+  protected Object convertValue(final Class<?> type, final String value)
+  {
+    Object newValue = value;
+    if (type != String.class) {
+      if (Map.class.isAssignableFrom(type)) {
+        newValue = new HashMap<String, Object>();
+
+        final String[] keyValues = value.split(",");
+        for (int i = 0; i < keyValues.length; i++) {
+          final String[] s = keyValues[i].split("=");
+          if (s[1].endsWith(".class")) {
+            ((Map) newValue).put(
+              s[0],
+              createTypeFromPropertyValue(
+                Class.class,
+                s[1].substring(0, s[1].indexOf(".class"))));
+          } else if (s[1].startsWith("[")) {
+            ((Map) newValue).put(
+              s[0],
+              createTypeFromPropertyValue(Class.class, s[1]));
+          } else {
+            ((Map) newValue).put(s[0], s[1]);
+          }
+        }
+      } else {
+        newValue = convertSimpleType(type, value);
+      }
+    }
+    return newValue;
+  }
+}
---
+++ ldaptive/beans/tags/ldaptive-beans-1.0.6/src/test/java/org/ldaptive/beans/reflect/CustomObject.java
@@ -0,0 +1,78 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2014 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.beans.reflect;
+
+import org.ldaptive.io.ValueTranscoder;
+
+/**
+ * Interface for testing bean annotations.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public interface CustomObject
+{
+
+
+  /** Prepare this object for use; */
+  void initialize();
+
+
+  /** Transcoder that adds 'prefix-' to string values. */
+  static class PrefixStringValueTranscoder implements ValueTranscoder<String>
+  {
+
+    /** Default constructor. */
+    public PrefixStringValueTranscoder() {}
+
+
+    /**
+     * Constructor with a non string param.
+     *
+     * @param  i  ignored argument
+     */
+    public PrefixStringValueTranscoder(final int i) {}
+
+
+    @Override
+    public String decodeStringValue(final String value)
+    {
+      return value.replaceFirst("prefix-", "");
+    }
+
+    @Override
+    public String decodeBinaryValue(final byte[] value)
+    {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String encodeStringValue(final String value)
+    {
+      return String.format("prefix-%s", value);
+    }
+
+    @Override
+    public byte[] encodeBinaryValue(final String value)
+    {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Class<String> getType()
+    {
+      return String.class;
+    }
+  }
+}
