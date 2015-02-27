566fa5490674034cd3029a1b242a3c676363cd2e
dfisher <dfisher@749bb74e-9c82-11dd-a4cb-5bfb67625b15>
2015-02-27 19:59:42 +0000

    Tagging version 1.0.6

---
+++ ldaptive/jldap-provider/tags/ldaptive-jldap-1.0.6/src/main/java/org/ldaptive/provider/jldap/JLdapSSLConnection.java
@@ -0,0 +1,40 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2014 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package org.ldaptive.provider.jldap;
+
+import com.novell.ldap.LDAPConnection;
+
+/**
+ * JLDAP provider implementation of ldap operations over SSL.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class JLdapSSLConnection extends JLdapConnection
+{
+
+
+  /**
+   * Creates a new jldap ssl connection.
+   *
+   * @param  conn  ldap connection
+   * @param  pc  provider configuration
+   */
+  public JLdapSSLConnection(
+    final LDAPConnection conn,
+    final JLdapProviderConfig pc)
+  {
+    super(conn, pc);
+  }
+}
